5c3b9d46af9059795365e9d009e716da5fc6ce1d
Daniil <daniilalifanov33@mail.ru>
2023-03-09 19:58:32 +0300

    Initial commit

---
+++ app/src/main/java/com/example/myapplication_123/Fragment2.java
@@ -0,0 +1,15 @@
+package com.example.myapplication_123;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Fragment2 extends Fragment {
+
+
+}
